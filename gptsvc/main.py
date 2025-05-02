5513d38a7979fe93cfbdc90b2041a1b63385b77b
Nambi Sankaran <snambi@gmail.com>
2025-05-02 09:26:51 -0700

    Added helm charts and k8s yaml files

--- gptsvc/main.py
+++ gptsvc/main.py
@@ -3,6 +3,9 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 import logging
 
+import requests
+import json
+
 from gptsvc.config.logging_config import initialize_logger
 initialize_logger()
 
@@ -17,7 +20,7 @@ async def on_startup():
     logger.info("running startup tasks")
     
 async def on_shutdown():
-    logger.info("shuting down\n\n")
+    logger.info("shutting down\n\n")
  
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -31,6 +34,30 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(user_router)
+    
+@app.post("/chat")
+async def chat(input:str):
+    data = {
+                "model": "mistral-large-latest",
+                "messages": [
+                {
+                    "role": "user",
+                    "content": f"{input}"
+                }
+                ]
+            }
+    
+    headers = {
+                "Content-Type": "application/json",
+                "Accept" : "application/json",
+                "Authorization" : "Bearer D23yxyESwEK9bo3fG2iiSWMtvLy5N45e"
+            }
+    response = requests.post("https://api.mistral.ai/v1/chat/completions", 
+                             data=json.dumps(data), 
+                             headers=headers)
+    
+    logger.debug("f{resposne.status_code}")
+    return {"result": f"{response.json()}"}
     
 
 @app.get("/hello")
